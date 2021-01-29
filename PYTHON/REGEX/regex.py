d66825e42bc0fa043123494e861d666f3138b0dc
shills112000 <simon.hills@dcs.tech>
2021-01-29 09:42:16 +0000

    update

---
+++ PYTHON/REGEX/regex.py
@@ -0,0 +1,13 @@
+import re
+
+# \d = digit
+
+[]	A set of characters	"[a-m]"	
+\	Signals a special sequence (can also be used to escape special characters)	"\d"	
+.	Any character (except newline character)	"he..o"	
+^	Starts with	"^hello"	
+$	Ends with	"world$"	
+*	Zero or more occurrences	"aix*"	
++	One or more occurrences	"aix+"	
+{}	Exactly the specified number of occurrences	"al{2}"	
+|	Either or	"falls|stays"
