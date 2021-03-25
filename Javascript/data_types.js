1e2029043c194da9ea05e590cbec55cb6e7bf5cd
Simon Hills <simonhills11@gmail.com>
2021-03-25 11:39:38 +0000

    commited

---
+++ Javascript/data_types.js
@@ -0,0 +1,35 @@
+// data types
+
+// basic data types
+
+// integers
+var bankAccount  = 100;
+var deposit = 50;
+var total = bankAccount + deposit;
+
+// ** 2  :to the power
+// 15 % 1 :operations (mod) 
+
+// strings abd variables
+var greeting = "Welcome back";
+var name = " Simon";
+name[0] // get first element of string
+var length = 0
+length =name.length
+alert (greeting + name)
+
+
+//Boleans
+
+//true
+//false
+//undefined
+//nuklll
+
+// Javascript methods
+
+alert("hello") // alert method
+console.log("hello") //output to console
+prompt ("enter something")
+
+var age=prompt ("enter your age") // put prompt in variable
---
+++ Javascript/javascript_basics.js
@@ -0,0 +1,76 @@
+// This is a comment
+
+//Basic Data types
+
+// below are all treated as just numbers
+10
+20.2
+-13.4
+
+// math with numbers
+2 * 2
+2 + 2
+6 / 2
+5 - 1
+2**4  // 2 power of 4
+15  % 14 // mod
+
+
+// strings anything wrapped in quotes
+
+"hello"
+"10" // note it is a string not number
+
+"Django" + " is cool" // concatination
+
+"django".length // length of string
+
+"hello \n start a new line"  // \n newline
+"hello \t tab"  // \t tab
+"hello \"I'm in quotes\" I'm not in quotes"  // \" get quotes
+
+//indexing
+
+"hello"[0] // get first charector
+
+
+// booleans
+
+true
+false
+
+// undefined and null
+undefined
+null 
+
+// clear console 
+clear()
+
+
+// Variables
+
+// var varName = value;
+
+//use camelCase
+
+// basic data types
+
+var bankAccount  = 100;
+var deposit = 50;
+
+var total = bankAccount + deposit;
+
+var greeting = "Welcome back";
+var name = " Simon";
+var length = 0
+length=name.length
+alert (greeting + name + length)
+
+// Javascript methods
+
+alert("hello") // alert method
+console.log("hello") //output to console
+prompt ("enter something")
+
+var age=prompt ("enter your age") // put prompt in variable
+
