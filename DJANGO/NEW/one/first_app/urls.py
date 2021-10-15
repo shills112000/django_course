2ec4340f9ecde1246c04fdfbd563f867688cdf6e
shills112000 <simon.hills@dcs.tech>
2021-10-15 09:36:20 +0100

    added stuff

---
+++ DJANGO/NEW/one/first_app/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from first_app import views
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('index/',views.index,name='index'),
+]
